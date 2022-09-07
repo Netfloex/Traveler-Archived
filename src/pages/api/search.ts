938a61a0f2bd19a96488d43595e3f1c37228a1b6
Netfloex <38650595+Netfloex@users.noreply.github.com>
2022-09-08 00:56:52 +0200

    Improve error handling for `search` endpoint

--- src/pages/api/search.ts
+++ src/pages/api/search.ts
@@ -7,9 +7,13 @@ const Search: NextApiHandler = async (req, res) => {
 		return res.json({ error: true })
 	}
 
-	const query = req.query.q ?? "Amsterdam"
-
-	res.json(await search(query))
+	const query = req.query.q || "Amsterdam"
+	await search(query)
+		.then(res.json)
+		.catch((err) => {
+			res.json({ error: err.message })
+			throw err
+		})
 }
 
 export default Search
--- src/server/breng/endpoints/search.ts
+++ src/server/breng/endpoints/search.ts
@@ -5,6 +5,10 @@ import { Cacher } from "@server/lib/Cacher"
 const cache = new Cacher<SearchResponse>("search", brengApi)
 
 export const search = async (query: string): Promise<SearchResponse> => {
+	if (!query) {
+		throw new TypeError("search: query is undefined")
+	}
+
 	return await cache.request({
 		url: "travelplanner/geo/search",
 		searchParams: {
